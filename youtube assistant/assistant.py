3585eb1982b20670c9521aa8a0bc148ab9ccfe73
Vinlaw <vmudehwe3661@gmail.com>
2024-03-12 00:02:25 -0400

    Updated assistant and streamlit app code

--- youtube assistant/assistant.py
+++ youtube assistant/assistant.py
@@ -13,14 +13,57 @@ load_dotenv()
 
 embeddings = OpenAIEmbeddings()
 
+test_url = 'https://www.youtube.com/watch?v=K-nnzpgrzwM'
 #Create a vector database with the video transcript
 def vector_db_from_youtube(url : str) -> FAISS:
 
     loader = YoutubeLoader.from_youtube_url(url)
     transcript = loader.load()
+
+    #Split transcript into chunks so that token size is not exceeded
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap = 100)
     docs = text_splitter.split_documents(transcript)
 
     db = FAISS.from_documents(docs,embeddings)
 
     return db
+
+def get_response_from_query(db,query,k=4):
+    # text davinci has a max token size of 4097 tokens, so we can send 4 docs 
+    #since each congtains 1000 tokens. These are the top k documents
+    docs = db.similarity_search(query,k=k)
+    docs_page_content = " ".join([d.page_content for d in docs])
+
+    llm = OpenAI(model='text-davinci-003')
+
+    prompt = PromptTemplate(
+
+        input_variables = ['query','docs'],
+
+        template = """
+
+        You are a helpful assistant that that can answer questions about youtube videos 
+        based on the video's transcript.
+        
+        Answer the following question: {question}
+        By searching the following video transcript: {docs}
+        
+        Only use the factual information from the transcript to answer the question.
+        
+        If you feel like you don't have enough information to answer the question, say "I don't know".
+        
+        Your answers should be verbose and detailed.
+     """
+    )
+
+    chain = LLMChain(llm=llm, prompt=prompt)
+
+    response = chain.run(question=query, docs=docs_page_content)
+
+    #Alternative:
+    #response = chain({'query':query, 'docs':docs_page_content})
+
+    #Helps with removing new line characters and seperates content using whitespace
+    response = response.replace("\n","")
+
+    return response
--- youtube assistant/streamlit.py
+++ youtube assistant/streamlit.py
@@ -1,3 +1,32 @@
 import streamlit as st
+import textwrap
+import assistant as assist
 
-st.title("Youtube RAG Assistant")
+st.title("Youtube Assistant")
+
+
+with st.sidebar:
+    with st.form(key='my_form'):
+
+        youtube_url = st.sidebar.text_area(
+            label = 'What is the video URL',
+            max_chars = 50,
+            key = 'video url'
+        )
+
+        query = st.sidebar.text_area(
+            label = 'Ask me about the video',
+            max_chars = 50,
+            key = 'query'
+        )
+
+        submit_button = st.form_submit_button(label = "Submit")
+
+if query and youtube_url:
+    db = assist.vector_db_from_youtube(youtube_url)
+    response, docs = assist.get_response_from_query(db=db,query=query)
+    st.subheader("Answer")
+    st.text(
+        textwrap.fill(response,width=80)
+    )
+    
